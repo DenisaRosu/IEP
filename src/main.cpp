52a8d4e91c967734f8a244d09a049d71e2b45455
Denisa <rosu_rebeca_denisa@yahoo.com>
2021-11-04 10:15:11 +0200

    First Item (4) in main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
-#include <iostream> 
+#include <iostream>
 
-int main (){
-    std::cout << "hello world";
-    return 0;
+class House
+{
+
+private:
+    int bedroom, bathroom, kitchen;
+
+public:
+    House()
+    { //constructor neinit.
+    }
+
+    House(int a, int b, int c) 
+    //initializare inainte de a intra in functie. 
+       :bedroom(a), bathroom(b),kitchen(c){}
+   
+    int roomNumber()
+    {
+        return bedroom+bathroom+kitchen;
+    }
+
+};
+
+int main()
+{ 
+    House house1;
+    House house2=House(3,2,1);
+
+    std::cout<<"Numarul de camere din casa = "<<house1.roomNumber()<<"\n";
+     std::cout<<"Numarul de camere din casa = "<<house2.roomNumber()<<"\n";
 }
